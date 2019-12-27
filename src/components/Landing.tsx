d1c204255266a404486349c74f55619842e5afd1
Shryans Goyal <shryansgoyal@gmail.com>
2019-12-27 23:38:28 +0530

    Working the photo on landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import {Box, Grid} from 'grommet';
+import {Box, Grid, Heading, Text} from 'grommet';
+import First from './first.jpg';
   
 const rows:Array<string> = ['flex'];
 const columns:Array<string> = ['flex', 'flex'];
 
 const About = (props:any) => (<Box
     pad = "xlarge"
+    responsive= {true}
+    // elevation= "small"
+    margin= "medium"
+    background= '#ff7d28'
+    {...props}
     >
-    Hi, this is Shryans. I'm a junior at Rice University studying Computer Science.
+    <Text size= "large" color='light-1'>Hi, this is Shryans. I'm a junior at Rice University studying Computer Science.
     I'm interested in PM, AI and Data Science. I love learning about the intersection
     of CS and othr fields.
+    </Text>
 </Box>);
 
 const Landing: React.FC = () => {
@@ -20,11 +27,15 @@ const Landing: React.FC = () => {
        columns={columns}
        gap="medium"
        justifyContent = {"center"}
+       style={{background: '#ff7d28'}}
        fill = {true}>
             <Box> 
-                <About/>
+                <About />
             </Box>
-            <Box background={"dark-1"}>Hi hello</Box>
+            <Box background='white'
+            pad= 'large'>
+                <img src={First}></img>
+            </Box>
        </Grid>
     </div>
   
